1c713d4b111bf888b3720cbfe2fac0fd9058fadd
TioLaba <151744456+TioLaba@users.noreply.github.com>
2023-12-15 11:42:34 -0300

    Update aula15.py

--- aula15.py
+++ aula15.py
@@ -2,7 +2,7 @@ def tabuada(x):
     tabuada = []
     for i in range(1, 11):
         tabuada.append(str(x) + ' X ' + str(i) + ' = ' + str(x*i))
-        return tabuada
+    return tabuada
 
 for x in tabuada(1):
     print(x)
